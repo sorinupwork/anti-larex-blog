da9a80f6007a90c20766a5d5ca4519c7c722c9e1
sorinupwork <sorin.upwork@gmail.com>
2022-12-20 14:19:29 +0200

    configure sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
+
+export default defineConfig({
+  basePath: "/studio",
+  name: "BLOG_Content_Studio",
+  title: "anti larex blog studio",
+  projectId,
+  dataset,
+
+  plugins: [deskTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+});
