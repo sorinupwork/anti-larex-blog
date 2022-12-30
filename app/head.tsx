df42e7e6cbb2cd5aa2aa236b82ac96f79abb5de1
sorinupwork <sorin.upwork@gmail.com>
2022-12-30 21:59:58 +0200

    add meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,39 @@ export default function Head() {
     <>
       <title>Illegal Recruitment Holland</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="title" content="Illegal Recruitment Holland"></meta>
+      <meta
+        name="description"
+        content="Campanie pro-legalitate, dezvaluim secretele ascunde ale companiilor olandeze de recrutare cei care nu platesc salariile sau concediile plus altele necorespunzator ./.  Pro-legality campaign, we reveal the hidden secrets of Dutch recruitment companies, those who do not pay salaries or holidays plus other improper"
+      ></meta>
       <link rel="icon" href="/favicon.ico" />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://anti-larex-blog.vercel.app/" />
+      <meta property="og:title" content="Illegal Recruitment Holland" />
+      <meta
+        property="og:description"
+        content="Campanie pro-legalitate, dezvaluim secretele ascunde ale companiilor olandeze de recrutare cei care nu platesc salariile sau concediile plus altele necorespunzator ./.  Pro-legality campaign, we reveal the hidden secrets of Dutch recruitment companies, those who do not pay salaries or holidays plus other improper"
+      />
+      <meta
+        property="og:image"
+        content="https://i.ibb.co/PjHWWQK/illegal-recruitment.png"
+      ></meta>
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta
+        property="twitter:url"
+        content="https://anti-larex-blog.vercel.app/"
+      />
+      <meta property="twitter:title" content="Illegal Recruitment Holland" />
+      <meta
+        property="twitter:description"
+        content="Campanie pro-legalitate, dezvaluim secretele ascunde ale companiilor olandeze de recrutare cei care nu platesc salariile sau concediile plus altele necorespunzator ./.  Pro-legality campaign, we reveal the hidden secrets of Dutch recruitment companies, those who do not pay salaries or holidays plus other improper"
+      />
+      <meta
+        property="twitter:image"
+        content="https://i.ibb.co/PjHWWQK/illegal-recruitment.png"
+      />
     </>
   );
 }
